56b9f8a090bd1044b1d4a5766d4bae1c5ba7f8f6
dodeca05 <kim565898@gmail.com>
2023-08-27 00:36:00 +0900

    도메인 생성 필요한 그래들 추가

---
+++ src/main/java/com/study/blog/domain/Article.java
@@ -0,0 +1,29 @@
+package com.study.blog.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+public class Article {
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id",updatable = false)
+    private Long id;
+
+    @Getter
+    @Column(name="title",nullable = false)
+    private String title;
+
+    @Getter
+    @Column(name="content", nullable = false)
+    private String content;
+    @Builder
+    public Article(String title, String content){
+        this.title=title;
+        this.content=content;
+    }
+    protected Article(){}
+}
