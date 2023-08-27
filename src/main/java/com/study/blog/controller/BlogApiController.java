2c0e27b3e37c1fb59fe43165f10c77ddd80ee00c
dodeca05 <kim565898@gmail.com>
2023-08-27 23:59:12 +0900

    블로그 글 조회 API 작성

--- src/main/java/com/study/blog/controller/BlogApiController.java
+++ src/main/java/com/study/blog/controller/BlogApiController.java
@@ -2,13 +2,17 @@ package com.study.blog.controller;
 
 import com.study.blog.domain.Article;
 import com.study.blog.dto.AddArticleRequest;
+import com.study.blog.dto.ArticleResponse;
 import com.study.blog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,5 +24,11 @@ public class BlogApiController {
     {
         Article saveArticle=blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(saveArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>>findAllArticles(){
+        List<ArticleResponse>articles=blogService.findAll().stream().map(ArticleResponse::new).toList();
+        return ResponseEntity.ok().body(articles);
     }
 }
